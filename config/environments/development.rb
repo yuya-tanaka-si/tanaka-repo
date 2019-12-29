b80dba87ef9c4d1c8ab8d5ca4fdd34c7a540ab40
yuya_tanaka <yuya.tanaka@sharing-innovations.co.jp>
2019-12-29 16:30:38 +0900

    host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,12 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+
+
+  config.hosts << "eziezi.com"
+
+
+
   # Do not eager load code on boot.
   config.eager_load = false
 
